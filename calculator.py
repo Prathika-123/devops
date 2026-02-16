60c9ced1f924e2adef2c0de7913872973db5a537
Prathika <kulalprathika70@gmail.com>
2026-02-16 12:36:17 +0530

    Update calculator.py

--- calculator.py
+++ calculator.py
@@ -11,3 +11,8 @@ def divide(a, b):
     if b == 0:
         raise ValueError("Cannot divide by zero")
     return a / b
+
+def modulus(a, b):
+    if b == 0:
+        raise ValueError("Cannot take modulus by zero")
+    return a % b
